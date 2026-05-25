1f3bcc21c8472567242b54b7c9f8293937aed83f
102guss <brown_321@live.com>
2026-05-25 15:43:12 -0600

    se agrega componente Course en App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Course from "./components/Course"
 import Header from "./components/Header"
 
 
@@ -7,6 +8,23 @@ function App() {
     <>
       <h1>Estoy en mi proyecto 3 de monedas</h1>
       <Header />
+      <Course
+        title="Curso de React"
+        description="Aprende React dede cero"
+      />
+      <Course
+        title="Curso de TypeScript"
+        description="Aprende TypeScript desde cero"
+      />
+
+      <Course
+        title="Curso de Angular"
+        description="Aprende Angular desde cero"
+      />
+      <Course
+        title="Curso de Vue"
+        description="Aprende Vue.js desde cero"
+      />
     </>
   )
 }
