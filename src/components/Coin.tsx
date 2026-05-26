9e01a0664dd586faf4629ad36682f30837f35509
102guss <brown_321@live.com>
2026-05-25 23:50:54 -0600

    Se agregó logica y botón para  agregar o quitar de favoritos

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,22 +1,27 @@
+import { useState } from "react";
 import type { CoinProps } from "../interfaces/CoinProps";
 
+const Coin = ({ order, name, symbol, price, priceChange, code }: CoinProps) => {
+  const [isFavorite, setIsFavorite] = useState(false);
 
+  const addToFavorites = () => {
+    setIsFavorite(!isFavorite);
+    // alert("Añadido a favoritos");
+  };
 
+  return (
+    <div>
+      <h1>{order}</h1>
+      <h2>{name}</h2>
+      <p>{symbol}</p>
+      <p>{price}</p>
+      <p>{priceChange}</p>
+      <p>{code}</p>
+      <button onClick={addToFavorites}>
+        {isFavorite ? "Quitar de favoritos" : "Agregar a favoritos"}
+      </button>
+    </div>
+  );
+};
 
-const Coin = ({order, name, symbol, price, priceChange, code}: CoinProps) => {
-      const addToFavorites = () => { if(name === "Bitcoin") { alert("Bitcoin es la mejor criptomoneda") } }
-    return (
-        <div>
-            <h1>{order}</h1>
-            <h2>{name}</h2>
-            <p>{symbol}</p>
-            <p>{price}</p>
-            <p>{priceChange}</p>
-            <p>{code}</p>
-             <button onClick={addToFavorites}>Agregar a favoritos</button>
-        </div>
-    );
-}
-
-
-export default Coin
+export default Coin;
