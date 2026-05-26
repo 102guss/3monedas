cf180ba07c762c224e7f57365379c10f40b4f1ed
102guss <brown_321@live.com>
2026-05-25 20:27:57 -0600

    Se agega botón con lógica a plantilla de moneda dentro de Coin

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -4,6 +4,7 @@ import type { CoinProps } from "../interfaces/CoinProps";
 
 
 const Coin = ({order, name, symbol, price, priceChange, code}: CoinProps) => {
+      const addToFavorites = () => { if(name === "Bitcoin") { alert("Bitcoin es la mejor criptomoneda") } }
     return (
         <div>
             <h1>{order}</h1>
@@ -12,6 +13,7 @@ const Coin = ({order, name, symbol, price, priceChange, code}: CoinProps) => {
             <p>{price}</p>
             <p>{priceChange}</p>
             <p>{code}</p>
+             <button onClick={addToFavorites}>Agregar a favoritos</button>
         </div>
     );
 }
