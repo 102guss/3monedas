22bbb0bb8ccf9f5aafa4eab61ae43195ee965a58
102guss <brown_321@live.com>
2026-05-28 18:12:02 -0600

    Se le cambian algunas propiedades al compoentes Coin por unas de la API para jalar la info restante

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import type { CoinProps } from "../interfaces/Coin";
 
-const Coin = ({ order, name, symbol, price, priceChange, code }: CoinProps) => {
+const Coin = ({ name, symbol, current_price, price_change_percentage_24h, image }: CoinProps) => {
 
   const [isFavorite, setIsFavorite] = useState<boolean>(false);
 
@@ -11,18 +11,23 @@ const Coin = ({ order, name, symbol, price, priceChange, code }: CoinProps) => {
 
   return (
    <tr className="coin-card [&>td]:px-4 [&>td]:py-3">
-      <td>{order}</td>
-      <td>
-        <div className="flex items-center gap-3">
-          <img src={symbol} alt={code} className="w-10 h-10 rounded-full" />
-          <div>
-            <span className="font-semibold">{name}</span>
-            <span className="text-sm text-gray-500">{code}</span>
+    <td className="px-6 py-4 text-sm text-gray-600">-</td>
+      <td className="px-6 py-4">  
+              <div className="flex items-center gap-3">
+          <img src={image} alt={symbol} className="w-10 h-10 rounded-full" />
+          <div className="flex flex-col">
+  <span className="font-semibold text-gray-900">{name}</span>
+  <span className="text-sm text-gray-500">{symbol}</span>
           </div>
         </div>
+
+
       </td>
-      <td>{price}</td>
-      <td>{priceChange}</td>
+      <td>${current_price.toFixed(2)}</td>
+      <td className={price_change_percentage_24h >= 0 ? "text-green-500" : "text-red-500"}>
+        {price_change_percentage_24h >= 0 ? "+" : ""}{price_change_percentage_24h.toFixed(2)}%
+      </td>
+      
       <td>
         <button onClick={handleFavorites}>
           {isFavorite ? "Quitar de favoritos" : "Agregar a favoritos"}
