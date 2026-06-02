d2febde6ad676b5b20d396156d92294f7901af47
102guss <brown_321@live.com>
2026-06-02 16:39:18 -0600

    Se agrega comopnente Not Found cuando la ruta es inválida}

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import CoinsContainer from "./components/CoinsContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
+import NotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<CoinsContainer />} />
+          <Route path="*" element={<NotFound />}/>
         </Route>
       </Routes>
     </BrowserRouter>
