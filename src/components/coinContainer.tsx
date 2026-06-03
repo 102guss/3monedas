73e17c5221c5604c5de23c51d8a2355209407f16
102guss <brown_321@live.com>
2026-06-02 21:44:59 -0600

    Se agregaron estados para cargar o lanzar un error amtes de renderizar la vista correspondiente, en lugar de empezar en nulo

--- src/components/coinContainer.tsx
+++ src/components/coinContainer.tsx
@@ -3,28 +3,41 @@ import { COINGECKO_API_KEY, URL_API, URL_COINS } from "../constants/api";
 import { useParams } from "react-router-dom";
 
 const CoinContainer = () => {
-  const [Coin, setCoin] = useState(null);
-   const { id } = useParams()
+    const [Coin, setCoin] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+    const { id } = useParams()
 
-  useEffect(() => {
-    fetch(
-      `${URL_API}/${URL_COINS}&x_cg_demo_api_key=${COINGECKO_API_KEY}&ids=${id}`,
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        setCoin(data[0]);
-      })
-      .catch((error) => {
-        console.error("Error al obtener los datos:", error);
-      });
-  }, [id]);
+    useEffect(() => {
+        fetch(
+            `${URL_API}/${URL_COINS}&x_cg_demo_api_key=${COINGECKO_API_KEY}&ids=${id}`,
+        )
+            .then((response) => response.json())
+            .then((data) => {
+                setCoin(data[0]);
+                setLoading(false)
+            })
+            .catch((error) => {
+                console.error("Error al obtener los datos:", error);
+                setError(error)
+                setLoading(false)
+                return <div>Error: {error}</div>
+            });
+    }, [id]);
+    if (loading) {
+        return <div>Loading...</div>
+    }
 
-  return (
-    <div>
-      <h1>Coin Container</h1>           
-      <p>{JSON.stringify(Coin)}</p>
-    </div>
-  );
+    if (error) {
+        return <div>Error: {error}</div>
+    }
+
+    return (
+        <div>
+            <h1>Coin Container</h1>
+            <p>{JSON.stringify(Coin)}</p>
+        </div>
+    );
 };
 
 export default CoinContainer;
