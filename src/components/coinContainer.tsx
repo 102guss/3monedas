32f1126ce5f30164e952babcb90955f3a57b9616
102guss <brown_321@live.com>
2026-06-02 20:47:44 -0600

    Se hace la peticion a la primer moneda de la API y se muestran sus datos crudos

--- src/components/coinContainer.tsx
+++ src/components/coinContainer.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from "react";
+import { COINGECKO_API_KEY, URL_API, URL_COINS } from "../constants/api";
+
 const CoinContainer = () => {
-    return (
-        <div>
-            <h1>Coin Container</h1>
-        </div>
+  const [Coin, setCoin] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      `${URL_API}/${URL_COINS}&x_cg_demo_api_key=${COINGECKO_API_KEY}&ids=bitcoin`,
     )
-}
+      .then((response) => response.json())
+      .then((data) => {
+        setCoin(data[0]);
+      })
+      .catch((error) => {
+        console.error("Error al obtener los datos:", error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Coin Container</h1>
+      <p>{JSON.stringify(Coin)}</p>
+    </div>
+  );
+};
 
 export default CoinContainer;
